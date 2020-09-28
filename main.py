6b014530e5a078d819b102d28f3fd9619a053f4c
Joshua <joshuamdk6@gmail.com>
2020-09-28 15:35:06 +1000

    Adding second page

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template("index.html")
+def home():
+    return render_template("home.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
